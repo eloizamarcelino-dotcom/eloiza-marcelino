5838b13a0fb27f26db09b2d1a5159327dee09896
eloizamarcelino-dotcom <eloiza_marcelino@estudante.sesisenai.org.br>
2026-08-12 21:25:34 -0300

    Update metodocomparametro.java
    mais um do dia 12

---
+++ metodos/src/metodocomparametro.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class metodocomparametro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        desempenharCalculo(5,4,2);
+    }
+    pubic static void desempenharCalculo(int n1,int n2, int n3){
+        int result = (n1+n2)*n3;
+        System.out.println(result);
+    }
+}
