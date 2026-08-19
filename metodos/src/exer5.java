7680b44b0a4cae91c0e97b3c7ad9a6c9354af89a
eloizamarcelino-dotcom <eloiza_marcelino@estudante.sesisenai.org.br>
2026-08-19 18:59:54 -0300

    Create exer5.java

---
+++ metodos/src/exer5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exer5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insia um numero base : ");
+        int numero = sc.nextInt();
+
+        System.out.println("insira o numero 1 ");
+        int num1 = sc.nextInt();
+
+        System.out.println("insira o numero 2 ");
+        int num2 = sc.nextInt();
+
+    }
+    public static void numeropertobase (int base , int num1 , int num2);
+
+}
