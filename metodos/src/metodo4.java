4d23081c8ee3ef95da6a3271f2ec97baa63ebd00
eloizamarcelino-dotcom <eloiza_marcelino@estudante.sesisenai.org.br>
2026-08-18 21:27:44 -0300

    Create metodo4.java

---
+++ metodos/src/metodo4.java
@@ -0,0 +1,24 @@
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
+public class metodo4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int vetor[] = {1, 2 , 3 , 4};
+
+
+    }
+    public static void imprimirmaior(int vetor[]){
+        double soma = 0;
+        int maiorValor = Integer.MIN_VALUE;
+        for (int i=0;i < vetor.length ; i++) {
+    soma += vetor[i];
+}
+        double media = soma / vetor.length;
+        System.out.println("A média dos valores é : " + media);
+
+    }
+    public static void imprimirmenor(int menor){
+
+    }
+}
