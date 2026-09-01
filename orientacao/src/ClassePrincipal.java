82c0c28120be9508907c44a00eb9d47f6403cc7c
eloizamarcelino-dotcom <eloiza_marcelino@estudante.sesisenai.org.br>
2026-09-01 20:22:01 -0300

    a
    a

---
+++ orientacao/src/ClassePrincipal.java
@@ -0,0 +1,8 @@
+public class ClassePrincipal {
+    public static void main(String[] args) {
+        perimetro p1 = new perimetro();
+        p1.artura = 3;
+        p1.largura = 2;
+        System.out.println(p1.obterPerimetro());
+    }
+}
---
+++ orientacao/src/perimetro.java
@@ -0,0 +1,9 @@
+public class perimetro {
+    double largura ;
+    double artura;
+
+
+    public double obterPerimetro(){
+        return (largura*2) + (artura*2);
+    }
+}
