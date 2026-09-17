a3d231e0b45cd5860f5fc965619aada9735ac8f5
eloizamarcelino-dotcom <eloiza_marcelino@estudante.sesisenai.org.br>
2026-09-16 21:18:50 -0300

    ae
    ae

--- orientacao/src/Recolhimento.java
+++ orientacao/src/Recolhimento.java
@@ -1,2 +1,29 @@
 public class Recolhimento {
+   /* Para efetuar o recolhimento do Imposto de Renda a Receita Federal tem o NOME, CPF, UF (RS,
+                                                                                           PR e SC) e RENDA ANUAL.
+    EX:
+    Nome: João da Silva
+    CPF: 123.456.789-00
+    UF: PR
+    RendaAnual: R$40.000
+    Para o cálculo do imposto a pagar de cada contribuinte, considere o seguinte:
+    Renda Anual Alíquota
+0 a 4.000 0%
+            4.001 a 9.000 5,8%
+            9.001 a 25.000 15%
+            25.001 a 35.000 27,5%
+    acima de 35.000 30%
+    Sendo assim, deve-se calcular o imposto a pagar do seguinte modo:
+    Imposto a pagar = Renda Anual * Alíquota
+    Crie 5 objetos da classe Contribuinte e coloque-os em um vetor.*/
+    private String nome ;
+    private double cpf;
+    private String UF;
+    private double RendaAnual;
+
+
+    if(RendaAnual >= 0 & RendaAnual =<4000){
+
+    }
+
 }
